34139dfb359ec9180df07251c3806d8c6763d74e
Ismael Marcos Martínez <ismael.marcos@nan-tic.com>
2026-09-09 14:18:16 +0200

    Add authors on www.article

--- cms.py
+++ cms.py
@@ -291,6 +291,8 @@ class Article(ModelSQL, ModelView):
         ondelete='CASCADE')
     category = fields.Many2One('www.article.category', 'Category',
         ondelete='SET NULL')
+    author = fields.Many2One('www.article.author', 'Author',
+        ondelete='SET NULL')
     text = fields.Text('Text', translate=True)
     image = fields.Many2One('www.file', 'Image', ondelete='SET NULL')
     uris = fields.Function(fields.One2Many('www.uri', None, 'URIs'),
@@ -310,6 +312,12 @@ class Article(ModelSQL, ModelView):
     def set_uris(cls, articles, name, value):
         # Prevent NotImplementedError for the function One2Many field.
         pass
+
+
+class ArticleAuthor(ModelSQL, ModelView):
+    __name__ = 'www.article.author'
+
+    name = fields.Char('Name', required=True)
 
 
 class ArticleCategory(tree(separator=' / '), ModelSQL, ModelView):
